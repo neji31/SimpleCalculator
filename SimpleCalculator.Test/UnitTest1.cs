d8794c27e975bfb899941e3ddaddc8b07f16d0b6
Abdechafi Neji <abdechafi.neji1993@outlook.com>
2023-07-20 09:55:43 +0100

    Add project files.

---
+++ SimpleCalculator.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using SimpleCalculator.Services;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace SimpleCalculator.Test
+{
+    public class UnitTest1
+    {
+       
+
+        [Theory]
+        [InlineData("4", "3", "7")]
+        [InlineData("20.3", "3.7", "24")]
+        [InlineData("-309", "-101", "-410")]
+        [InlineData("9", "-9", "0")]
+        public void ShouldAddTwoNumbers(string x, string y, string expected)
+        {
+            //Act
+            string actual = Calculator.Calculer(x,y);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ SimpleCalculator/Services/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Services
+{
+    public class Calculator
+    {
+        public static string Calculer(string texta, string textb)
+        {
+            double a = double.Parse(texta);
+            double b = double.Parse(textb);
+            return (a + b).ToString();
+        }
+    }
+}
